48ead5f5e20a185c534175387e0123e78ec00c8c
machadinhos <pbmachado2002@gmail.com>
2023-08-27 21:37:27 +0100

    Initial comit

---
+++ src/test/java/com/machapipo/hotelAPI/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machapipo.hotelAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

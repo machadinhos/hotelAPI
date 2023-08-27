c9b65c208009e175d1207d81f47cb487aed6ad3d
machadinhos <pbmachado2002@gmail.com>
2023-08-27 21:40:37 +0100

    Code reformat

--- src/main/java/com/machapipo/hotelAPI/HotelApiApplication.java
+++ src/main/java/com/machapipo/hotelAPI/HotelApiApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HotelApiApplication.class, args);
-	}
+    public static void main (String[] args) {
+
+        SpringApplication.run(HotelApiApplication.class, args);
+    }
 
 }

604c9c26fc4628e28f013aee5c38d59cbe339745
machadinhos <pbmachado2002@gmail.com>
2023-08-28 09:18:27 +0100

    Initial controllers created

---
+++ src/main/java/com/machapipo/hotelAPI/controller/rest/GuestController.java
@@ -0,0 +1,10 @@
+package com.machapipo.hotelAPI.controller.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/api/guests", "/api/guest"})
+public class GuestController {
+
+}
---
+++ src/main/java/com/machapipo/hotelAPI/controller/rest/RoomController.java
@@ -0,0 +1,10 @@
+package com.machapipo.hotelAPI.controller.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/api/rooms", "/api/room"})
+public class RoomController {
+
+}
